c85d9d12a1f71b9418c1756752ea0d80620baa6e
Selina Sharma <selinasharma040699@gmail.com>
2022-07-21 16:33:59 +0530

    Add project files.

---
+++ CustomLoginPage/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CustomLoginPage.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
